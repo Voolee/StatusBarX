728c40ecd874516050fec253012917b16e125c62
Владимир Беляев <b.2540@ya.ru>
2023-05-01 17:20:43 +0300

    added CPU GPU temp and fan

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import psutil
 import rumps
 
@@ -12,8 +13,18 @@ class SystemMonitorApp(rumps.App):
         cpu_percent = psutil.cpu_percent()
         memory_info = psutil.virtual_memory()
         memory_percent = memory_info.percent
+        cpu_temp = self.get_info('CPU die temperature')
+        gpu_temp = self.get_info('GPU die temperature')
+        fan_speed = self.get_info('Fan')
 
-        self.title = f"CPU: {cpu_percent}% | RAM: {memory_percent}%"
+        self.title = f"CPU: {cpu_percent}% | RAM: {memory_percent}% | CPU Temp: {cpu_temp}°C | GPU Temp: {gpu_temp}°C | Fan: {fan_speed} rpm"
+
+    def get_info(self, info_type):
+        data = [each.strip() for each in (os.popen('sudo powermetrics --samplers smc -i1 -n1')).read().split('\n') if each != '']
+        for line in data:
+            if info_type in line:
+                return line.strip(f'{info_type}: ').rstrip(' C').rstrip(' rpm')
+        return f'{info_type} not found'
 
 if __name__ == "__main__":
     app = SystemMonitorApp()
